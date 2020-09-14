6e3e2fb5c72cd56139e961994f36fbf66894c975
song.shi <song.shi@atzuche.com>
2020-09-14 18:36:55 +0800

    first commit

---
+++ src/main/java/com/ss/zookeeper/example/QuartzConfig.java
@@ -0,0 +1,10 @@
+package com.ss.zookeeper.example;
+
+/**
+ * Quartz
+ *
+ * @author shisong
+ * @date 2020/9/14
+ */
+public class QuartzConfig {
+}
---
+++ src/main/java/com/ss/zookeeper/example/ZookeeperLeaderApplication.java
@@ -0,0 +1,13 @@
+package com.ss.zookeeper.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZookeeperLeaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZookeeperLeaderApplication.class, args);
+    }
+
+}
